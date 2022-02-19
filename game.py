e9b7d49e9986a1f8365adb5667e2e0e216dae5bc
Bartłomiej Konecki <bartekkonecki97@gmail.com>
2022-02-19 10:51:23 +0100

    v0.003 chose class, new dev functions
    New functions error_wrong_input, debug, work_in_progress, init_starting_variables.
    Tweaked the ascii dragon.
    Added chosing of class after selecting new game

--- game.py
+++ game.py
@@ -1,31 +1,71 @@
+def debug(input):
+    print("Debug result:")
+    print(input)
+
+
+def work_in_progress():
+    print("Creation in progress\nThank You for trying!")
+
+def init_starting_variables():
+    # 1 = Warrior
+    # 2 = Mage
+    # 3 = Rogue
+    # -1 = NA
+    global class_dict
+    class_dict = {'1': "Warrior", '2': "Mage", '3': 'Rogue'}
+    global player_class
+    player_class = "-1"
+
+
+def error_wrong_input():
+    print("Incorrect command!\nTry again!")
+
+
 def new_game():
-    print("Creation in prgoress\nThank You for trying!")
+    global player_class
+    while player_class == "-1":
+        print("Please select You class")
+        print("1 = Warrior")
+        print("2 = Mage")
+        print("3 = Rogue")
+        user_input = input()
+        if (user_input == "1") | (user_input == "2") | (user_input == "3"):
+            player_class = user_input
+            debug(player_class)
+        else:
+            error_wrong_input()
+    work_in_progress()
+
 
 def load_game():
-    print("Creation in prgoress\nThank You for trying!")
+    work_in_progress()
+
 
 def leave_game():
     print("See You next time!")
     exit()
 
+
 def main():
+    init_starting_variables()
+
     print("""
         ,  ,
           \\\\ \\\\           
           ) \\\\ \\\\    _p_ 
-          )^\\))\\))  /  *\ 
-           \_|| || / /^`-' 
-  __       -\ \\\\--/ / 
+          )^\\))\\))  /  *\\ 
+           \\_|| || / /^`-' 
+  __       -\\ \\\\--/ / 
 <'  \\\\___/   ___. )'
-     `====\ )___/\\\\ 
+     `====\\ )___/\\\\ 
           //     `"
           \\\\    /  \\
           `"
 Welcome in the RPG_Game
-v0.002""".center(40))
-    while(True):
+v0.003""")
+    while True:
         print("\nRPG_Game")
-        print("1 = New Game (comming soon)")
+        print("1 = New Game (comming sooner)")
         print("2 = Load Game (comming soon)")
         print("3 = Leave game")
         user_input = input()
@@ -36,9 +76,8 @@ v0.002""".center(40))
         elif user_input == "3":
             leave_game()
         else:
-            print("Incorrect command!\nTry again!", user_input)
+            error_wrong_input()
 
 
 if __name__ == "__main__":
     main()
-
