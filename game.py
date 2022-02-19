cc7561c446be6025cb54f5e9871f4f00e8abc379
Bartłomiej Konecki <bartekkonecki97@gmail.com>
2022-02-19 10:15:41 +0100

    Main menu implementation
    Main menu is working now. We have 'implement soon' for new game and load game commands. Exit is working properly

--- game.py
+++ game.py
@@ -1,3 +1,13 @@
+def new_game():
+    print("Creation in prgoress\nThank You for trying!")
+
+def load_game():
+    print("Creation in prgoress\nThank You for trying!")
+
+def leave_game():
+    print("See You next time!")
+    exit()
+
 def main():
     print("""
         ,  ,
@@ -12,7 +22,21 @@ def main():
           \\\\    /  \\
           `"
 Welcome in the RPG_Game
-v0.001""".center(40))
+v0.002""".center(40))
+    while(True):
+        print("\nRPG_Game")
+        print("1 = New Game (comming soon)")
+        print("2 = Load Game (comming soon)")
+        print("3 = Leave game")
+        user_input = input()
+        if user_input == "1":
+            new_game()
+        elif user_input == "2":
+            load_game()
+        elif user_input == "3":
+            leave_game()
+        else:
+            print("Incorrect command!\nTry again!", user_input)
 
 
 if __name__ == "__main__":
